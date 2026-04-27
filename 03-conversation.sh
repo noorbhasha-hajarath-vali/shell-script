eead80e7f129e1c7cf0713324238d33357d486d3
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-04-27 16:07:41 +0530

    File 2 Created

---
+++ 03-conversation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Variable declaration
+person1="Basha"
+person2="Vali"
+
+echo "${person1}: Hello ${person2}, how are you?"
+echo "${person2}: Hi ${person1}, I'm doing well, thank you! How about you?"
+echo "${person1}: I'm good too, thanks for asking ${person2}!"
+echo "${person2}: What are your plans for the weekend, ${person1}?"
+echo "${person1}: Let's go hiking together, ${person2}! It will be fun!"
+echo "${person2}: That sounds great, ${person1}! I'm looking forward to it!"
+
