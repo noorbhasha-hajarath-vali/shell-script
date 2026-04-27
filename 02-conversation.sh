4cde71e9f6688e4e4e91966b2da32a32b3eab8ff
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-04-27 16:04:04 +0530

    File 2 Created

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Basha: Hello Vali, how are you?"
+echo "Vali: Hi Basha, I'm doing well, thank you! How about you?"
+echo "Basha: I'm good too, thanks for asking Vali!"
+echo "Vali: What are your plans for the weekend, Basha?"
+echo "Basha: Let's go hiking together, Vali! It will be fun!"
+echo "Vali: That sounds great, Basha! I'm looking forward to it!"
+
