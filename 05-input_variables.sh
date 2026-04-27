6ff145b2f973545e4c61b8c69a4f224f64732712
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-04-27 17:30:30 +0530

    new file added

---
+++ 05-input_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter the name of the first name:"
+read first_name
+
+echo "Please enter the name of the last name:"
+read last_name
+
+echo "Hello ${first_name} ${last_name}, welcome to the world of Shell Scripting!"
