c3cc8506cb647b2a8c796d9ec8f3933a31e47b9b
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-04-29 12:41:26 +0530

    all files are modified

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,8 +2,10 @@
 
 # These are already existing in default, we just call them
 
+
+# sh file-name Vali Noor Basha Hajarath --> it will show the arguements
 echo "all variables passed through this script: $@"
-echo ""
-# echo "all variables passed through this script: $*"
+echo " "
+echo "all variables passed through this script: $*"
 
-
+# 
