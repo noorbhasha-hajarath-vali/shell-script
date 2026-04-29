96e79593163dbe036e7c0112da4a1e4f2367fd13
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-04-29 12:33:34 +0530

    all files are modified

--- 07-run-cmd-variables.sh
+++ 07-run-cmd-variables.sh
@@ -5,3 +5,13 @@
 DATE=$(date +%Y-%m-%d)
 
 echo "My cousin born today, i.e. ${DATE}"
+echo ""
+
+CURRENT_PATH=$(pwd)
+
+echo -e "Current path is:\n${CURRENT_PATH}"
+echo ""
+
+FILES_LIST=$(ls)
+
+echo -e "List of files in the directory is: \n${FILES_LIST}"
---
+++ 08-special-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# These are already existing in default, we just call them
+
+echo "all variables passed through this script: $@"
+echo ""
+# echo "all variables passed through this script: $*"
+
+
