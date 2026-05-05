f19c1e9ffdfa96ba7dce6b888e0c26eb48f8bde0
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-05-05 23:58:56 +0530

    Push Code to github

---
+++ 06-File-logs/01-save-files-output-logs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+YELLOW="\e[33m"
+RESET="\e[0m"
+LOG_DIR="/var/log/shell-script"
+SCRIPT_NAME=$( echo "$0 | cut -d "." -f1 ) # deleting .sh extension
+LOG_FILE="$LOG_DIR/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FILE
+echo "Script started executing at $(date)"
+
+if [ ${USER_ID} -ne 0 ]; then
+    echo "Error: User don't have sudo access, still you can run with sudo"
+    exit 1
+fi
+
+VALIDATE(){
+    dnf install $1 -y &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "Error: $1 installation failed, try again later"
+        exit 1
+    else
+        echo "Success: $1 successfully installed"
+    fi
+}
+
+INSTALLATION(){
+    PKG_NAME=$1
+
+    dnf list installed $PKG_NAME &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$PKG_NAME not installed, installing now"
+        VALIDATE $PKG_NAME
+    else
+        echo "$PKG_NAME already installed ... $YELLOW SKIPPING $RESET"
+    fi
+}
+
+INSTALLATION tree
+INSTALLATION nginx
+INSTALLATION docker
