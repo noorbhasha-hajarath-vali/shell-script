6c06eb2dfa72901d0ccd644355ab974e4a8e2468
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-05-05 11:40:10 +0530

    Push Code to github

--- 04-Functions/01-functions.sh
+++ 04-Functions/01-functions.sh
@@ -4,6 +4,6 @@ USER_ID=$(id -u)
 
 if [ ${USER_ID} -ne 0 ]; then
     echo "Error: User doesn't have root permissions, use sudo"
+else
+    echo echo "Success: User have root access, lets continue"
 fi
-
-
