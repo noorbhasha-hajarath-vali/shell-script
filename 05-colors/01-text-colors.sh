5a9a0df28260f814c146d8751ccebf015bfb9fe7
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-05-05 22:56:23 +0530

    Push Code to github

--- 05-colors/01-text-colors.sh
+++ 05-colors/01-text-colors.sh
@@ -8,9 +8,11 @@ BLUE="\e[34m"
 MAGENTA="\e[35m"
 CYAN="\e[36m"
 WHITE="\e[37m"
-RESET="\e[0m"
 
 echo -e "$BLACK this is the black color"
+
+echo "Hi Basha, How are you"
+
 echo -e "$RED this is the Red color"
 echo -e "$GREEN this is the GREEN color"
 echo -e "$YELLOW this is the YELLOW color"
@@ -18,4 +20,3 @@ echo -e "$BLUE this is the BLUE color"
 echo -e "$MAGENTA this is the MAGENTA color"
 echo -e "$CYAN this is the CYAN color"
 echo -e "$WHITE this is the WHITE color"
-echo -e "$RESET this is the RESET color"
